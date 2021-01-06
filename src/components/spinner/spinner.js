2a891b2ad0e3226d0f12a2571b299321a845ca44
Polina Liutsko <polina.liutsko@gmail.com>
2021-01-06 21:25:30 +0200

    Loading indicator logic implemented

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-eclipse-481yxsk8ukg">
+            <div class="ldio-9zs62cx173r">
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
